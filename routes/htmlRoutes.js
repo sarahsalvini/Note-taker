1277ee60bd7a467096f72aa46bd4b1825044f7b2
sarahsalvini <salvini.sarah.ss@gmail.com>
2020-02-27 15:38:24 -0600

    added htmlroutes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,1 +1,8 @@
 var path = require("path");
+var router = require('express').Router()
+
+router.get('/notes', function(req, res){
+    res.sendFile(path.join(__dirname, '../public/notes.html'))
+})
+
+module.exports = router
